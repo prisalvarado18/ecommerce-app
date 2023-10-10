cdeaec1c0c6040c3aea8aab2d761c1ae695b90e7
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-10-10 12:50:25 -0500

    Added Forms

--- client/src/scenes/checkout/Confirmation.jsx
+++ client/src/scenes/checkout/Confirmation.jsx
@@ -1,5 +1,17 @@
+import { Box } from '@mui/material';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+
 const Confirmation = () => {
-	return <div>Confirmation</div>;
+	return (
+		<Box m='90px auto' width='80%' height='50vh'> 
+			<Alert severity='success'>
+				<AlertTitle>Success</AlertTitle>
+				You have successfully make an order -{' '}
+				<strong>Congrats on making your purchase</strong>
+			</Alert>
+		</Box>
+	);
 };
 
 export default Confirmation;
